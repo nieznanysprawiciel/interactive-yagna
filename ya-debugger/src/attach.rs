12948c647fc95b9e391cead127fed06cf75a2eb9
nieznany.sprawiciel <witek@golem.network>
2020-12-17 18:37:05 +0100

    Move ya-debugger to other repository

--- ya-debugger/src/attach.rs
+++
@@ -1,8 +0,0 @@
-use yarapi::rest::activity::DefaultActivity;
-use yarapi::rest::Session;
-
-pub async fn attach_debugger(session: Session, activity: DefaultActivity) -> anyhow::Result<()> {
-    drop(session);
-    drop(activity);
-    unimplemented!()
-}
